fcf72848eae8d19fab02715f0596e2d6deea3bfc
Bohdan Chuprynka <chuprinkabogdan223@gmail.com>
2026-03-08 12:45:35 -0400

    Update portfolio

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,6 +5,7 @@ import stock_sales from '../assets/images/stock_sales.jpg';
 import persona_gpt from '../assets/images/PersonaGPT.jpg';
 import sentiment_project from '../assets/images/sentiment_project.png';
 import tableau_project from '../assets/images/tableau_vis.png';
+import rag_project from "../assets/images/rag_architecture.png";
 
 
 import '../assets/styles/Project.scss';
@@ -16,11 +17,17 @@ function Project() {
         <div className="projects-grid">
 
         <div className="project">
+                <a href="https://github.com/BohdanChuprynka/Sentiment-Analysis-Model" target="_blank" rel="noreferrer"><img src={rag_project} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/BohdanChuprynka/Sentiment-Analysis-Model" target="_blank" rel="noreferrer"><h2>Policy Retrieval Augmented Generation (RAG) System</h2></a>
+                <p>Engineered a Policy Retrieval-Augmented Generation (RAG) System for grounded question answering over policy documents using hybrid retrieval, score fusion, and citation-aware LLM generation. The system integrates document ingestion, chunking, embedding-based semantic search, lexical retrieval, and evidence-driven response synthesis within a modular FastAPI architecture. Built with a production-minded design, it demonstrates practical experience in retrieval systems, backend API development, LLM orchestration, and transparent AI application engineering.
+                </p>
+            </div>
+
+        <div className="project">
                 <a target="_blank" rel="noreferrer"><img src={vertebrae_proj} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a target="_blank" rel="noreferrer"><h2>Vertebrae Image Segmentation</h2></a>
                 <p>Designed and developed a <strong>private</strong> end-to-end medical imaging pipeline using PyTorch and nnU-Net architecture to automatically segment vertebrae in spinal X-rays, generate anatomical labels, and compute alignment angles to support clinical diagnosis and surgical planning.</p>
         </div>
-
 
         <div className="project">
                 <a href="https://public.tableau.com/app/profile/bohdan.chuprynka/viz/BusinessSummary_17527722001530/BusinessSummary" target="_blank" rel="noreferrer"><img src={tableau_project} className="zoom" alt="thumbnail" width="100%"/></a>
@@ -46,7 +53,7 @@ function Project() {
             </div>
         </div>
     </div>
-    );
+    );  
 }
 
 export default Project;
